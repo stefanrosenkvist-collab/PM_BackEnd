cc6b450af4bf44c58cd85368a259f4d236540728
stefanrosenkvist-collab <stefan.rosenkvist@gmail.com>
2025-12-16 17:41:00 +0100

    Add export for CustomersService in CustomersModule

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -8,6 +8,7 @@ import { Customer } from '../entities/customer.entity';
   imports: [TypeOrmModule.forFeature([Customer])],
   controllers: [CustomersController],
   providers: [CustomersService],
+  exports: [CustomersService],
 })
 export class CustomersModule {}
 
